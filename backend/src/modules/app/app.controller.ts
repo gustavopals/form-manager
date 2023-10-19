0e28bdf036ceb92e17a8175cfe1bba2125cd4fa6
Gustavo Pals <gustavoandrepals@gmail.com>
2023-10-19 16:58:49 -0300

    feat: hello world

--- backend/src/modules/app/app.controller.ts
+++ backend/src/modules/app/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from '../../shared/services/app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Res() res: Response) {
+    return res.json(this.appService.getHello());
   }
 }
