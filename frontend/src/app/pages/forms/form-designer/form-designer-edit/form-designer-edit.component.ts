8a0f143002eee79ae541f92382eb10f1e461340d
Gustavo Pals <gustavoandrepals@gmail.com>
2023-10-24 17:21:04 -0300

    feat: new form designer

--- frontend/src/app/pages/forms/form-designer/form-designer-edit/form-designer-edit.component.ts
+++ frontend/src/app/pages/forms/form-designer/form-designer-edit/form-designer-edit.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PoBreadcrumb, PoNotificationService } from '@po-ui/ng-components';
+import { RestService } from 'src/app/services/rest.service';
 
 @Component({
   selector: 'app-form-designer-edit',
@@ -7,4 +11,54 @@ import { Component } from '@angular/core';
 })
 export class FormDesignerEditComponent {
 
+  myForm!: FormGroup;
+  id!: string | null;
+  breadcrumb: PoBreadcrumb = {
+    items: [
+      { label: 'Home', action: () => this.router.navigate(['/']) },
+      { label: 'Form Designer', action: () => this.router.navigate(['/form-designer']) },
+      { label: 'Edit' }
+    ]
+  }
+
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private rest: RestService,
+    private poNotification: PoNotificationService,
+  ) { }
+
+  ngOnInit() {
+    this.id = this.activatedRoute.snapshot.paramMap.get("id")
+
+    this.myForm = this.formBuilder.group({
+      tableName: ['', [Validators.required]],
+      code: ['', [Validators.required]],
+      captionBr: ['', [Validators.required]],
+      captionEn: ['', [Validators.required]],
+      captionEs: ['', [Validators.required]],
+    });
+  }
+
+  save() {
+    if (this.myForm.valid) {
+      const data = this.myForm.value;
+      if (this.id) data.id = this.id;
+
+      this.rest.post('/forms/form-designer', data).subscribe((res: any) => {
+        console.log(res);
+        this.poNotification.success('Saved successfully!');
+        this.router.navigate(['/form-designer']);
+      }, (err: any) => {
+        this.poNotification.error('Error saving');
+        console.log(err);
+      }
+      );
+    }
+  }
+
+  cancel() {
+    this.router.navigate(['/form-designer']);
+  }
 }
